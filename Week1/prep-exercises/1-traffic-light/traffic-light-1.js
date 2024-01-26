6da8c1ebfa39f6b2ec675e026d9d91efe7e6b6b4
xrustik666 <kilorust@gmail.com>
2024-01-26 22:23:23 +0100

    traffic-light-1 is done

--- Week1/prep-exercises/1-traffic-light/traffic-light-1.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-1.js
@@ -8,10 +8,22 @@ const trafficLight = {
 };
 
 let rotations = 0;
+
 while (rotations < 2) {
   const currentState = trafficLight.state;
-  console.log("The traffic light is on", currentState);
 
+  if (currentState === "green") {
+    trafficLight.state = "orange";
+  } else if (currentState === "orange") {
+    trafficLight.state = "red";
+  } else if (currentState === "red") {
+    trafficLight.state = "green";
+    
+    rotations++;
+  }
+
+  console.log("The traffic light is on", currentState);
+  
   // TODO
   // if the color is green, turn it orange
   // if the color is orange, turn it red
